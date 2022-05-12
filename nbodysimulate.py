8fc819ac4d610856a5b889226120bc8bb55b1bb5
Callum <Callumr714@icloud.com>
2022-05-12 18:57:24 +0100

    Force calculations

--- nbodysimulate.py
+++ nbodysimulate.py
@@ -6,6 +6,7 @@ from astropy import constants as const
 class Body:
     G = const.G
     AU = const.AU
+    TIMESTEP = 3600 * 24
 
     def __init__(self, x, y, radius, mass, color):
         self.x = x
@@ -18,3 +19,19 @@ class Body:
 
         self.xv = 0
         self.xy = 0
+
+    def force(self, obj):
+        obj_x = obj.x
+        obj_y = obj.y
+        obj_dist_x = obj_x - self.x
+        obj_dist_y = obj_y - self.y
+
+        dist = math.sqrt(obj_dist_x ** 2 + obj_dist_y ** 2)
+        force = self.G * self.mass * obj.mass / dist ** 2
+
+        angle = math.atan2(obj_dist_y, obj_dist_x)
+
+        force_x = math.cos(angle) * force
+        force_y = math.sin(angle) * force
+
+        return force_x, force_y
