c1ab5b2e90fc483ca90ae942d0ef79a882f7b407
Callum <Callumr714@icloud.com>
2022-05-15 15:39:18 +0100

    seperate render

--- nbody.py
+++ nbody.py
@@ -7,8 +7,7 @@ import random
 pygame.init()
 
 
-WIDTH, HEIGHT = 2240,1260
-WIN = pygame.display.set_mode((WIDTH, HEIGHT), RESIZABLE)
+
 
 class Nbody:
     G = 6.67428e-11
@@ -84,91 +83,4 @@ class Nbody:
 
 
 
-def run_SOL():
-    run = True
-    clock = pygame.time.Clock()
 
-    SUN = Body(0, 0, 15, 1.98892 * 10 ** 30, (255,165,0))
-    SUN2 = Body(2 * Body.AU, 2, 8, 1.98892 * 10 ** 30, (255, 165, 0))
-    SUN2.yv = 200.236 * 1000
-    EARTH = Body(-1 * Body.AU, 2, 4, 5.9742 * 10**24,(0,0,255))
-    EARTH.yv = 29.783 * 1000
-    MARS = Body(-1.524 * Body.AU, 0, 3, 6.39 * 10**23, (255,25,0))
-    MARS.yv = 24.077 * 1000
-    VENUS = Body(0.723 * Body.AU, 0, 3.5, 4.865 * 10**24, (255,255,255))
-    VENUS.yv = -35.02 * 1000
-
-    MOON = Body(-1.02 * Body.AU, 2, 2, 1.73477e22, (150,150,150))
-    MOON.yv = 2.9783e4
-
-    asteroid = Body(5 * Body.AU, 2, 1, 5 * 10**17, (100,100,100))
-    asteroid.yv = 10.40 * 1000
-
-    MERCURY = Body(0.387 * Body.AU, 0, 2, 3.30 * 10**23 ,(150,150,150))
-    MERCURY.yv = -47.4 * 1000
-
-    JUPITER = Body(-5.203 * Body.AU, 0, 8, 1.9 *10 ** 27, (125,84,84))
-    JUPITER.yv = 13.07 * 1000
-
-    SATURN = Body(-9.5 * Body.AU, 0, 6, 5.683 * 10 ** 26, (255,253,208))
-    SATURN.yv = 9.69 * 1000
-
-    bodies = [SUN, MARS, VENUS, MERCURY, EARTH, asteroid, JUPITER, SATURN]
-    i = 0
-    while run:
-        clock.tick(2400)
-        WIN.fill((0, 0, 0))
-        fps = int(clock.get_fps())
-
-
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        for body in bodies:
-            body.position(bodies)
-            body.plot(WIN)
-
-        pygame.display.update()
-    pygame.quit()
-
-
-
-def Run_galaxu():
-    run = True
-    clock = pygame.time.Clock()
-
-    B_HOLE = Body(2 * Body.AU,0, 20, 2*10**30, (0,0,0))
-
-
-    B_HOLE2 = Body(0, 0, 12, 2 * 10 **30, (9, 0, 0))
-
-
-    bodies = [B_HOLE2]
-
-    for i in range(5, 15):
-        x = (-1 + i * 0.15)
-        y = 0
-
-        generated = Body(x * Body.AU, y * Body.AU, 5, 0.01 * 10 ** 24, (200, 200, 255))
-        generated.yv = 25  * 1000
-        bodies.append(generated)
-
-
-
-    while run:
-        clock.tick(120)
-        WIN.fill((25, 25, 25))
-
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        for body in bodies:
-            body.position(bodies)
-            body.plot(WIN)
-        pygame.display.update()
-    pygame.quit()
-
-
-run_SOL()
