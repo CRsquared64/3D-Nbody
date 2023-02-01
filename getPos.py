a2693edb5975e373ebab0c1b21ebfa3632feb78d
Callum <Callumr714@icloud.com>
2023-02-01 18:36:33 +0000

    Inital Copy paste function

---
+++ getPos.py
@@ -0,0 +1,38 @@
+# here jon, will this make you HAPPY?!?!?!?!?
+from nbody import Nbody
+from pynndescent import NNDescent
+
+import pickle
+import sim.solarSystem
+
+import numpy as np
+from tqdm import tqdm
+
+
+def getPos(bodies, cycles):
+    batches = 4
+    batch_size = cycles / batches
+    points = [(body.x, body.y, body.z) for body in bodies]
+
+    points_array = np.array(points)
+
+    nn = NNDescent(points_array)
+    vid_id = sim.solarSystem.video_name
+
+    poses = [[] for i in range(len(bodies))]
+    amount = len(bodies) * cycles
+
+    n = 0
+    print("Calculating Positions")
+    with tqdm(total=amount) as pb:
+        for i in range(cycles):
+            n = n + 1
+            for n, body in enumerate(bodies):
+                body.position(bodies, nn)
+                poses[n].append(body.get_draw_pos())
+                pb.update(1)
+            if i % batch_size == 0:
+                with open('')
+
+
+getPos(sim.solarSystem.bodies, 128)
