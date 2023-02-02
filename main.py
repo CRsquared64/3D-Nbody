8850c81d9141b8f83df3f336c3194e640d3f28de
Callum <Callumr714@icloud.com>
2023-02-02 17:10:27 +0000

    Main Function started

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ import sim.solarSystem
 import sim.earthMoonSystem
 import sim.plutoCharonSystem
 
+CYCLES = 1024
+BODIES = sim.solarSystem.bodies
+
+VID_ID = sim.solarSystem.video_name
+
+load = True
+
+if __name__ == '__main__':
+    if load != True:
+        getPos.getPos(BODIES, CYCLES)
+    else:
+
