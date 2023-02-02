8548cde45e7e9f72a7eac4c88b13f0b181754810
Callum <Callumr714@icloud.com>
2023-02-02 17:25:27 +0000

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sys
+import os
+import pygame
+import shutil
 
 import getPos
 import renderBodies
@@ -7,6 +10,21 @@ import fileHandler
 import sim.solarSystem
 import sim.earthMoonSystem
 import sim.plutoCharonSystem
+
+try:
+    os.mkdir('run')
+except OSError as error:
+    print('Emptying old cache...')
+    shutil.rmtree('run')
+    os.mkdir('run')
+
+WIDTH, HEIGHT = 1920, 1080
+FPS = 60
+
+pygame.font.init()
+
+font = pygame.font.Font(pygame.font.get_default_font(), 12)
+i_font = pygame.font.Font(pygame.font.get_default_font(), 24)
 
 CYCLES = 1024
 BODIES = sim.solarSystem.bodies
@@ -17,9 +35,7 @@ load = True
 file = sys.argv
 
 if __name__ == '__main__':
-    if load != True:
-        getPos.getPos(BODIES, CYCLES)
+    if not load:
+        poses = getPos.getPos(BODIES, CYCLES)
     else:
-        fileHandler.fileLoad()
-
-
+        poses = fileHandler.fileLoad(file)
